6bbb2e6309bac1aea6e31461cb6eff315995f3a2
Anne <anneomclaughlin@gmail.com>
2017-07-09 21:23:57 -0700

    Add project files.

---
+++ ReverseString/ArrayAndStringHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayAndStringHelpers
+{
+    public static class StringHelpers
+    {
+        public static string ReverseString(string test)
+        {
+            if (String.IsNullOrWhiteSpace(test) || test.Length < 2)
+            {
+                return test;
+            }
+
+            char[] testArray = test.ToCharArray();
+            for (int i = 0; i < (test.Length / 2); i++)
+            {
+                char temp = testArray[i];
+                testArray[i] = testArray[test.Length - i - 1];
+                testArray[test.Length - i - 1] = temp;
+            }
+
+            return new String(testArray);
+        }
+    }
+}
---
+++ Test/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static ArrayAndStringHelpers.StringHelpers;
+
+namespace Test
+{
+    [TestClass]
+    public class ReverseString
+    {
+        [TestMethod]
+        public void TestReverseString()
+        {
+            CreateReverseStringTest(null, null);
+            CreateReverseStringTest("", "");
+            CreateReverseStringTest("a", "a");
+            CreateReverseStringTest("ab", "ba");
+            CreateReverseStringTest("cat", "tac");
+            CreateReverseStringTest("happie", "eippah");
+        }
+
+        void CreateReverseStringTest(string test, string expected)
+        {
+            string actual = ReverseString(test);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
