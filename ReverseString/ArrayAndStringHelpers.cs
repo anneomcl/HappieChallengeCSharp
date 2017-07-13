f7932d39ef660f621b48b7c9ff883c15f3def440
Anne <anneomclaughlin@gmail.com>
2017-07-12 21:22:01 -0700

    IsPermutation problem complete

--- ReverseString/ArrayAndStringHelpers.cs
+++ ReverseString/ArrayAndStringHelpers.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArrayAndStringHelpers
 {
     public static class StringHelpers
     {
-        public static string ReverseString(string test)
+        static void ReverseCharArray(char[] testArray)
+        {
+            int length = testArray.Length;
+
+            for (int i = 0; i < (length / 2); i++)
+            {
+                char temp = testArray[i];
+                testArray[i] = testArray[length - i - 1];
+                testArray[length - i - 1] = temp;
+            }
+        }
+
+        public static string ReverseString(string test, bool useBuiltIn)
         {
             if (String.IsNullOrWhiteSpace(test) || test.Length < 2)
             {
@@ -12,14 +25,86 @@ namespace ArrayAndStringHelpers
             }
 
             char[] testArray = test.ToCharArray();
-            for (int i = 0; i < (test.Length / 2); i++)
+
+            if (useBuiltIn)
             {
-                char temp = testArray[i];
-                testArray[i] = testArray[test.Length - i - 1];
-                testArray[test.Length - i - 1] = temp;
+                Array.Reverse(testArray);
+            }
+            else
+            {
+                ReverseCharArray(testArray);
             }
 
             return new String(testArray);
+        }
+
+        public static bool hasAllUniqueCharacters(string test)
+        {
+            if (String.IsNullOrWhiteSpace(test) || test.Length < 2)
+            {
+                return true;
+            }
+
+            bool[] uniqueChecker = new bool[256];
+
+            char[] testArray = test.ToCharArray();
+
+            foreach (char letter in testArray)
+            {
+                if (uniqueChecker[letter])
+                {
+                    return false;
+                }
+
+                uniqueChecker[letter] = true;
+            }
+
+            return true;
+        }
+
+        public static bool isPermutation(string input, string test)
+        {
+            if (String.IsNullOrWhiteSpace(input) || input == test)
+            {
+                return true;
+            }
+
+            Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+            foreach (char letter in input)
+            {
+                if (letterCounts.ContainsKey(letter))
+                {
+                    letterCounts[letter]++;
+                }
+                else
+                {
+                    letterCounts.Add(letter, 1);
+                }
+            }
+
+            Dictionary<char, int> testLetterCounts = new Dictionary<char, int>();
+            foreach (char letter in test)
+            {
+                if (testLetterCounts.ContainsKey(letter))
+                {
+                    testLetterCounts[letter]++;
+                }
+                else
+                {
+                    testLetterCounts.Add(letter, 1);
+                }
+            }
+
+            foreach (char item in letterCounts.Keys)
+            {
+                if (!testLetterCounts.ContainsKey(item)
+                    || letterCounts[item] != testLetterCounts[item])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
--- Test/UnitTest.cs
+++ Test/UnitTest.cs
@@ -5,8 +5,29 @@ using static ArrayAndStringHelpers.StringHelpers;
 namespace Test
 {
     [TestClass]
-    public class ReverseString
+    public class StringAndArrayTests
     {
+        [TestMethod]
+        public void TestIsPermutation()
+        {
+            CreateIsPermutationTest(null, null, true);
+            CreateIsPermutationTest("", "", true);
+            CreateIsPermutationTest("a", "a", true);
+            CreateIsPermutationTest("a", "b", false);
+            CreateIsPermutationTest("abc", "cab", true);
+            CreateIsPermutationTest("doggierjr", "rdjogrgei", true);
+            CreateIsPermutationTest("abc", "cab", true);
+            CreateIsPermutationTest("abec", "cadb", false);
+            CreateIsPermutationTest("eaaff", "cadb", false);
+            CreateIsPermutationTest("dabc", "cadb", true);
+        }
+
+        void CreateIsPermutationTest(string test, string input, bool expected)
+        {
+            bool actual = isPermutation(test, input);
+            Assert.AreEqual(expected, actual);
+        }
+
         [TestMethod]
         public void TestReverseString()
         {
@@ -16,11 +37,34 @@ namespace Test
             CreateReverseStringTest("ab", "ba");
             CreateReverseStringTest("cat", "tac");
             CreateReverseStringTest("happie", "eippah");
+            CreateReverseStringTest("happier", "reippah");
         }
 
         void CreateReverseStringTest(string test, string expected)
         {
-            string actual = ReverseString(test);
+            string actual = ReverseString(test, true);
+            Assert.AreEqual(expected, actual);
+            actual = ReverseString(test, false);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void TestUniqueCharactersInString()
+        {
+            CreateUniqueCharactersTest(null, true);
+            CreateUniqueCharactersTest("", true);
+            CreateUniqueCharactersTest("a", true);
+            CreateUniqueCharactersTest("ab", true);
+            CreateUniqueCharactersTest("aa", false);
+            CreateUniqueCharactersTest("Aa", true);
+            CreateUniqueCharactersTest("AA", false);
+            CreateUniqueCharactersTest("aBcD&*j", true);
+            CreateUniqueCharactersTest("aBcB&*j", false);
+        }
+
+        void CreateUniqueCharactersTest(string test, bool expected)
+        {
+            bool actual = hasAllUniqueCharacters(test);
             Assert.AreEqual(expected, actual);
         }
     }
